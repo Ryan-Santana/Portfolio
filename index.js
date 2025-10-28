da14a948d595202dd3b4c9b0c5cd1907104f48cc
Ryan Santana <ryansantana2003@hotmail.com>
2025-10-28 02:05:07 -0300

    Resp contatos, dropbox mobile

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// dropbox mobile
+const toggle = document.querySelector('.dropbox');
+const mobileMenu = document.querySelector('.menu-mobile');
+
+toggle.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+});
+// dropbox mobile
