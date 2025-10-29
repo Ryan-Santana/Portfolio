68618ea03bb358060073b0bb8461cc6b9bd85c1d
Ryan Santana <ryansantana2003@hotmail.com>
2025-10-29 18:34:44 -0300

    Download CV + ajustes

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ toggle.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
 });
 // dropbox mobile
+
+// botão curriculo
+let cvbutton = document.querySelector('.botao-cv')
+
+cvbutton.addEventListener('click', (downloadcv) => {
+  downloadcv.preventDefault();
+
+  if (confirm("Deseja baixar o PDF do currículo ?")) {
+  let pdf = document.createElement('a');
+  pdf.href = cvbutton.getAttribute('href');
+  pdf.download = cvbutton.getAttribute('download');
+  pdf.click(); 
+} 
+});
+
+
+// botão curriculo
